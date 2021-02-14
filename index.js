b59482d94210a37a73436581b50baa4b09b332de
ltslyc <lts.lyc@qq.com>
2021-02-14 10:46:43 +0800

    基本完成

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: lts
+ * @Date: 2021-02-12 14:07:28
+ * @LastEditTime: 2021-02-14 10:33:10
+ * @FilePath: \allocate-time\index.js
+ */
+const  getStartAndEnd = require('./lib/getStartAndEnd') 
+const  timeDisposal = require('./lib/timeDisposal')
+
+module.exports = allocateTime = { ...getStartAndEnd, ...timeDisposal }
